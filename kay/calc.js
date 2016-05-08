d668d8885751dbc931b9a9ad79e76f6cb5cfcc1c
K Kollmann <git@k.kollmann.moe>
2016-05-08 19:17:25 +0200

    basic calculations possible (not chained)

--- kay/calc.js
+++ kay/calc.js
@@ -1,29 +1,108 @@
+var result = 0;
+var currentValue = 0;
+var lastAction = "equals";
 
 // display the value of a number button on the calculator's display
-function display(el) {
-    var buttonValue = el.innerHTML;
-    document.getElementById("display").innerHTML = buttonValue;
+function update_display(value) {
+    display.innerHTML = value;
 }
 
-// clear the display
+function calculate(action) {
+    // la = action;
+    cv = Number(currentValue);
+
+    // debug
+    // alert("result before: " + result + "\ncurrent value before: " + cv);
+
+    switch(lastAction) {
+        case "add":
+            result = result + cv;
+            break;
+        case "subtract":
+            result = result - cv;
+            lastAction = "subtract";
+            break;
+        case "multiply":
+            result = result * cv;
+            lastAction = "multiply";
+            break;
+        case "divide":
+            result = result / cv;
+            lastAction = "divide";
+            break;
+        case "equals":
+            result = result + cv;
+            lastAction = "equals";
+            break;
+        case "clear":
+            update_display(0);
+            lastAction = "equals";
+            break;
+        default:
+            break;
+    }
+    currentValue = 0;
+    lastAction = action;
+    update_display(result);
+
+    // debug
+    // alert("result after: " + result + "\ncurrent value after: " + cv);
+}
+
+// string together digits entered one after the other
+function concatNumber(btn) {
+    var buttonValue = btn.innerHTML;
+    
+    if (currentValue == 0) {
+        currentValue = buttonValue;
+    } else {
+        currentValue = currentValue + buttonValue;
+    }
+
+    display.innerHTML = currentValue;
+}
+
+// clear the display, reset calculator to zero
 function clear() {
-    document.getElementById("display").innerHTML = 0;
+    result = 0;
+    currentValue = 0;
+    lastAction = "clear";
 }
 
 // wait until the site has finished loading
 document.addEventListener('DOMContentLoaded', function() {
 
-    var clear_btn = document.getElementById("clear_btn");
+    var display = document.getElementById("display");
+    display.innerHTML = currentValue;
+
+    var clear_btn = document.getElementById("clear");
     clear_btn.addEventListener('click', function() {
         clear();
+        calculate("equals");
     }, false);
 
-    // create array number buttons
-    // iterate over them, adding event listeners for click event
+    var calculate_btn = document.getElementById("calculate");
+    calculate_btn.addEventListener('click', function() {
+        calculate();
+        // result = 0;
+    }, false);
+
+    // create number buttons array
+    // iterate over no. buttons, adding event listeners for click event
     var num_btn_array = document.getElementsByClassName("number");
     for (var i = 0; i < num_btn_array.length; i++) {
         num_btn_array[i].addEventListener('click', function() {
-            display(this);
+            concatNumber(this);
         }, false);
     }
+
+    // create operation buttons array
+    // iterate over operation buttons, adding event listeners for click event
+    var op_btn_array = document.getElementsByClassName("operation");
+    for (var j = 0; j < op_btn_array.length; j++) {
+        op_btn_array[j].addEventListener('click', function() {
+            calculate(this.id);
+        }, false);
+    }
+
 });
